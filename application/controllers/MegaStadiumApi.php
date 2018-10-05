599b3c8d5891af312884f6051ef368b4ae975f6a
Lenwe <len_we@hotmail.com>
2018-10-04 21:05:03 -0300

    Initial commit

---
+++ application/controllers/MegaStadiumApi.php
@@ -0,0 +1,54 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MegaStadiumApi extends CI_Controller {
+
+	public function getTimes($dateInMillis){
+		$method = $_SERVER['REQUEST_METHOD'];
+		if($method != 'GET'){
+			json_output(array('status' => 400,'message' => 'Error de petición.'));
+		} else {
+			$this->load->model('MegaStadiumModel');
+	        $response = $this->MegaStadiumModel->getTimes($dateInMillis);
+
+			json_output($response);
+		}
+	}
+
+	public function getTableSheetReservations($dateInMillis){
+		$method = $_SERVER['REQUEST_METHOD'];
+		if($method != 'GET'){
+			json_output(array('status' => 400,'message' => 'Error de petición.'));
+		} else {
+			$this->load->model('MegaStadiumModel');
+	        $response = $this->MegaStadiumModel->getTableSheetReservations($dateInMillis);
+
+			json_output($response);
+		}
+	}
+
+	public function getContacts(){
+	$method = $_SERVER['REQUEST_METHOD'];
+		if($method != 'GET'){
+			json_output(array('status' => 400,'message' => 'Error de petición.'));
+		} else {
+			$this->load->model('MegaStadiumModel');
+	        $response = $this->MegaStadiumModel->getContacts();
+
+			json_output($response);
+		}
+	}
+
+	public function updateContacts(){
+
+	}
+
+	private function getBody(){
+		$stream_clean = $this->security->xss_clean($this->input->raw_input_stream);
+		return json_decode($stream_clean,true);
+	}
+
+
+}
+
+?>
